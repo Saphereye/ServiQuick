7cef1458eb9f0927e5718d74db7c1e55fbbc1e5c
Saphereye <adarshdas950@gmail.com>
2023-09-30 06:52:14 +0530

    fix: Routing gives intermediary points too

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from kivy.app import App
 from kivy_garden.mapview import MapView, MapMarkerPopup, MapMarker
 from kivy_garden.mapview.clustered_marker_layer import ClusteredMarkerLayer
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton, ToggleButtonBehavior
+from kivymd.uix.label import MDIcon
+from kivy.graphics import Line, Color
 import requests
 import wrapper
 
@@ -9,66 +13,128 @@ class MapWithRoute(BoxLayout):
     def __init__(self, **kwargs):
         super(MapWithRoute, self).__init__(**kwargs)
         self.orientation = 'vertical'
+        self.latlong = wrapper.get_lat_long()
 
         # Initialize the map
-        (lat, lon) = (float(i) for i in wrapper.get_lat_long().split(','))
+        (lat, lon) = (float(i) for i in self.latlong.split(','))
         self.mapview = MapView(zoom=10, lat=lat, lon=lon)  # Hyderabad coordinates
         self.add_widget(self.mapview)
 
-        # Request route information from HERE API
-        route_data = self.get_route_data()
-        if route_data:
-            # Extract coordinates from the route data (e.g., list of latitudes and longitudes)
-            route_coordinates = [(lat, lon) for lon, lat in route_data]
+        # Add buttons for Hospital, Firefighter, and Police
+        self.add_buttons()
 
-            # Draw the route on the map
-            self.draw_route(route_coordinates)
+        # Initialize route data with Hospital as the initial service
+        self.current_service = wrapper.Service.HOSPITAL
+        self.update_route()
 
-    def get_route_data(self):
-        # Make a request to HERE API to retrieve route data
-        # Replace with your HERE API request logic and URL
-        waypoint0 = wrapper.get_lat_long()
-        waypoint1 = wrapper.get_nearest_service(wrapper.Service.HOSPITAL)
-        mode = 'car'
-        app_id = 'OnyJ5cxLBpW9KOhx3wCW'
-        app_code = 'iWrpAat8eDeNt2CqqWtw6bQPweqd2xqCNk7wh9gR_p8'
-    
-        api_url = f'https://router.hereapi.com/v8/routes?transportMode={mode}&origin={waypoint0}&destination={waypoint1}&return=summary&apikey={app_code}'
-        print(f"{api_url=}")
+    def add_buttons(self):
+        # Create buttons for Hospital, Firefighter, and Police
+        # background_normal='./Flag_of_the_Red_Cross.svg.png'
+        self.hospital_button = ToggleButton(text="🏥", font_name="seguiemj", font_size='36sp', color='red')
+        self.firefighter_button = ToggleButton(text="🔥", font_name="seguiemj", font_size='36sp')
+        self.police_button = ToggleButton(text="👮", font_name="seguiemj", font_size='36sp')
 
-        response = requests.get(api_url)
-        print(f"{response.json()=}")
+        # Bind button press events to corresponding functions
+        self.hospital_button.bind(on_release=self.on_hospital_button_press)
+        self.firefighter_button.bind(on_release=self.on_firefighter_button_press)
+        self.police_button.bind(on_release=self.on_police_button_press)
 
-        if response.status_code == 200:
-            output = [(float(waypoint0.split(',')[0]), float(waypoint0.split(',')[1]))]
-            for i in range(len(response.json()['routes'][0]['sections'])):
-                output.append((
-                    response.json()['routes'][0]['sections'][i]['arrival']['place']['location']['lat'],
-                    response.json()['routes'][0]['sections'][i]['arrival']['place']['location']['lng']
-                ))
-            route_data = output
-            # route_data = [(i['lat'], i['lng']) for i in response.json()['sections']['arrival']['location']]
-            print(f"{route_data=}")
-            return route_data
-        else:
-            print('Failed to retrieve route data.')
-            return None
+        self.hospital_button.state = 'down'
+
+        # Add buttons to the layout
+        button_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.2))
+        button_layout.add_widget(self.hospital_button)
+        button_layout.add_widget(self.firefighter_button)
+        button_layout.add_widget(self.police_button)
+        self.add_widget(button_layout)
+
+    def on_hospital_button_press(self, instance):
+        self.current_service = wrapper.Service.HOSPITAL
+        self.hospital_button.state = 'down'
+        self.firefighter_button.state = 'normal'
+        self.police_button.state = 'normal'
+        self.update_route()
+
+    def on_firefighter_button_press(self, instance):
+        self.current_service = wrapper.Service.FIRE_STATION
+        self.hospital_button.state = 'normal'
+        self.firefighter_button.state = 'down'
+        self.police_button.state = 'normal'
+        self.update_route()
+
+    def on_police_button_press(self, instance):
+        self.current_service = wrapper.Service.POLICE
+        self.hospital_button.state = 'normal'
+        self.firefighter_button.state = 'normal'
+        self.police_button.state = 'down'
+        self.update_route()
+
+    def update_route(self):
+        # Clear existing markers and routes
+        # for child in self.mapview.children[:]:
+        #     if isinstance(child, MapMarker):
+        #         self.mapview.remove_marker(child)
+        # print(self.mapview.children[:])
+
+
+        # # Request route information from HERE API based on the current service
+        # waypoint0 = self.latlong
+        # waypoint1 = wrapper.get_nearest_service(self.current_service)
+        # mode = 'car'
+        # app_id = 'OnyJ5cxLBpW9KOhx3wCW'
+        # app_code = 'iWrpAat8eDeNt2CqqWtw6bQPweqd2xqCNk7wh9gR_p8'
+
+        # api_url = f'https://router.hereapi.com/v8/routes?transportMode={mode}&origin={waypoint0}&destination={waypoint1}&return=summary&apikey={app_code}'
+        # print(f"{api_url=}")
+
+        # response = requests.get(api_url)
+        # print(f"{response.json()=}")
+
+        # if response.status_code == 200:
+        #     output = [(float(waypoint0.split(',')[0]), float(waypoint0.split(',')[1]))]
+        #     for i in range(len(response.json()['routes'][0]['sections'])):
+        #         output.append((
+        #             response.json()['routes'][0]['sections'][i]['arrival']['place']['location']['lat'],
+        #             response.json()['routes'][0]['sections'][i]['arrival']['place']['location']['lng']
+        #         ))
+        #     route_data = output
+        #     print(f"{route_data=}")
+        #     self.draw_route(route_data)
+        # else:
+        #     print('Failed to retrieve route data.')
+        self.draw_route(wrapper.get_route(self.current_service))
 
     def draw_route(self, route_coordinates):
-        # # Draw the route using a Polyline
-        # polyline = MapMarkerPopup()
-        # polyline.source = './marker.png'  # You can use your own custom polyline image
-        # polyline.add_polyline(route_coordinates)
-        # self.mapview.add_marker(polyline)
-        layer = ClusteredMarkerLayer()
+        # Create a new marker layer
+        # layer = ClusteredMarkerLayer()
 
-        (lat, lon) = (float(i) for i in wrapper.get_lat_long().split(','))
-        layer.add_marker(lon, lat, cls=MapMarker)
+        # print(route_coordinates)
 
-        # print(help(layer.add_marker))
-        for lon, lat in route_coordinates:
-            layer.add_marker(lon=lon, lat=lat, cls=MapMarker)
-        self.mapview.add_widget(layer)
+        # # Add markers for the route
+        # for location in route_coordinates:
+        #     layer.add_marker(lon=location[0], lat=location[1], cls=MapMarker)
+
+        # # Add the marker layer to the map
+        # self.mapview.add_widget(layer)
+
+
+        # Color( 0, 0 , 0 , .8)
+        # Line(points=route_coordinates , width=4 , joint_presicion = 100)
+
+         # Create a Line object to draw the route
+        line = Line(points=[], width=4)
+        self.mapview.add_widget(line)
+
+        # Convert geographical coordinates to screen coordinates
+        screen_coordinates = []
+        for lat, lon in route_coordinates:
+            screen_x, screen_y = self.mapview.get_window_xy_from(lat, lon)
+            screen_coordinates.extend([screen_x, screen_y])
+
+        # Set the Line's points to draw the route
+        line.points = screen_coordinates
+
+
 
 class MapApp(App):
     def build(self):
--- wrapper.py
+++ wrapper.py
@@ -1,8 +1,10 @@
 import requests
 from enum import Enum
+import polyline
 
 # todo use environment variables
 API_KEY = 'iWrpAat8eDeNt2CqqWtw6bQPweqd2xqCNk7wh9gR_p8'
+ORS_API_KEY = '5b3ce3597851110001cf6248056ade7a5d63493cb05b0d6692675228'
 
 # ipinfo.io
 def get_lat_long() -> str:
@@ -27,6 +29,7 @@ def get_lat_long() -> str:
 class Service(Enum):
     HOSPITAL = 'hospital'
     FIRE_STATION = 'firestation'
+    POLICE = 'police'
 
 # https://discover.search.hereapi.com/v1/discover?at=52.8173086,12.2368342&limit=2&lang=en&q=hospital+hyderbad&apiKey=iWrpAat8eDeNt2CqqWtw6bQPweqd2xqCNk7wh9gR_p8
 def get_nearest_service(service: Service) -> str:
@@ -53,11 +56,72 @@ def get_nearest_service(service: Service) -> str:
     except requests.exceptions.RequestException as e:
         print(f"Request error: {e}")
 
-# 'https://router.hereapi.com/v8/routes?transportMode=car&origin=52.5308,13.3847&destination=52.5323,13.3789&return=summary&apikey={YOUR_API_KEY}'
-def get_route():
-    pass
+# # 'https://router.hereapi.com/v8/routes?transportMode=car&origin=52.5308,13.3847&destination=52.5323,13.3789&return=summary&apikey={YOUR_API_KEY}'
+# def get_route():
+#     waypoint0 = get_lat_long()
+#     waypoint1 = get_nearest_service(Service.HOSPITAL)
+#     mode = 'car'
+#     app_id = 'OnyJ5cxLBpW9KOhx3wCW'
+#     app_code = 'iWrpAat8eDeNt2CqqWtw6bQPweqd2xqCNk7wh9gR_p8'
+
+#     api_url = f'https://router.hereapi.com/v8/routes?transportMode={mode}&origin={waypoint0}&destination={waypoint1}&return=summary&apikey={app_code}'
+#     print(f"{api_url=}")
+
+#     response = requests.get(api_url)
+#     print(f"{response.json()=}")
+
+#     if response.status_code == 200:
+#         route_polyline = response.json()['routes'][0]['sections'][0]['polyline']
+#         route_coordinates = polyline.decode(route_polyline)
+
+#         # route_coordinates now contains the detailed list of coordinates along the route
+#         for lat, lon in route_coordinates:
+#             print(f"Latitude: {lat}, Longitude: {lon}")
+#     else:
+#         print('Failed to retrieve route data.')
+
+def get_route(service: Service) -> list[list[float]]:
+    start_coords = get_lat_long().split(',')
+    end_coords = get_nearest_service(service).split(',')
+
+    # Define the API endpoint URL
+    endpoint = 'https://api.openrouteservice.org/v2/directions/driving-car'
+
+    # Define the request parameters
+    params = {
+        'api_key': ORS_API_KEY,
+        'start': f'{start_coords[1]},{start_coords[0]}',  # Latitude, Longitude
+        'end': f'{end_coords[1]},{end_coords[0]}',  # Latitude, Longitude
+    }
+
+    try:
+        # Send a GET request to the API
+        response = requests.get(endpoint, params=params)
+
+        print(f"{response=}")
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+
+            # Extract the route geometry (list of coordinates)
+            if 'features' in data and len(data['features']) > 0:
+                route_geometry = data['features'][0]['geometry']['coordinates']
+                return route_geometry
+            else:
+                print('No route data found in the response.')
+                return None
+        else:
+            print(f'Failed to retrieve route data. Status code: {response.status_code}')
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print(f'Request error: {e}')
+        return None
 
 if __name__ == "__main__":
-    # print(get_lat_long())
-    print(get_nearest_service(Service.HOSPITAL))
-    print(get_nearest_service(Service.FIRE_STATION))
+    print(get_lat_long())
+    # print(get_nearest_service(Service.HOSPITAL))
+    # print(get_nearest_service(Service.FIRE_STATION))
+    print(get_route(Service.HOSPITAL))
